d48972d48451fd4854a78555f7d0339f623f6605
arzhed <arthursymon@gmail.com>
2014-03-07 14:18:11 -0700

    added list of users

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,28 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  if (req.session.user == undefined || req.session.pwd == undefined || req.session.uid==undefined) {
+		res.redirect('/');
+	}
+	else {
+		mysql = require('mysql');
+		conn = mysql.createConnection({
+			host: 'web2.cpsc.ucalgary.ca',
+			user: 's513_apsbanva',
+			password: '10037085',
+			database: 's513_apsbanva'
+		});
+		conn.connect();
+
+
+		var queryImage = 'SELECT username, uid FROM user';
+		conn.query(queryImage, function(err,rows) {
+			var userList = '';
+			for(var i=0;i<rows.length;i++){
+				userList += '<a href="/users/' + rows[i].uid
+							+'">'+rows[i].username +'</a></br>';
+			}
+			res.render('feed', {html : userList})
+		});
+	}
 };
