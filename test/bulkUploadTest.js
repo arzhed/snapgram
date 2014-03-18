7c5d9a950eae6e93f94fbd3e9c6f01bc82af3dd7
robin2309 <robin.besson1@gmail.com>
2014-03-17 22:10:05 -0600

    started writing up the schema for bulk upload testing (on users bulk upload) talk about it first before starting to code

--- test/bulkUploadTest.js
+++ test/bulkUploadTest.js
@@ -6,3 +6,11 @@ var assert = require("assert");
  	});
 
  });
+
+ //make a function into bulk.js for inserting users into DB
+ //test for file existing
+ //test for .json extension
+ //test for correct structure of the json
+ //test for error when trying to insert username that already exists
+ //test for error when trying to follow a uid that does not exist
+ //test for records well inserted into database
