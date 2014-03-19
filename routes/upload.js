4282e6e5e691465ed8d5f7cdc8679a01e8e58805
arzhed <arthursymon@gmail.com>
2014-03-19 12:54:21 -0600

    functional testing started

--- routes/upload.js
+++ routes/upload.js
@@ -1,7 +1,7 @@
 var sessions = require('./sessionIds');
 exports.newPicture = function(req, res){
 	if (!(sessions.sessionIds.indexOf(req.session.sessionId) > -1)) {
-		res.redirect('/');
+		res.redirect('/sessions/new');
 	}
 	else{
 		res.render('upload', {title: 'SNAPGRAM', name: req.session.user})
